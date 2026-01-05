01a10e1e018a63221eca6364c1bac29b47fe5ed2
1Ridwan <97636059+1Ridwan@users.noreply.github.com>
2026-01-05 04:49:11 +0000

    configure backend with s3 bucket

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-ridwan-ecs"
-    key            = "terraform.tfstate"
-    region         = "eu-west-2"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+    bucket       = "terraform-state-ridwan-ecs"
+    key          = "terraform.tfstate"
+    region       = "eu-west-2"
+    use_lockfile = true
+    encrypt      = true
   }
 }

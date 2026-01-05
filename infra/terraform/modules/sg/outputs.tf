433df41f35bec38f42e513187e3d564469960147
1Ridwan <97636059+1Ridwan@users.noreply.github.com>
2026-01-05 04:46:34 +0000

    add securty group for endpoints

--- infra/terraform/modules/sg/outputs.tf
+++ infra/terraform/modules/sg/outputs.tf
@@ -5,3 +5,7 @@ output "alb_sg_id" {
 output "ecs_service_sg_id" {
     value = aws_security_group.allow_container_port_traffic_from_alb.id
 }
+
+output "endpoint_sg_id" {
+    value = aws_security_group.endpoint_security_group.id
+}

d19ea27f9f473c1431706a7d3bf848b5548ad194
1Ridwan <97636059+1Ridwan@users.noreply.github.com>
2026-01-12 23:00:03 +0000

    use s3 native state locking

--- infra/global/backend/backend.tf
+++ infra/global/backend/backend.tf
@@ -31,16 +31,5 @@ resource "aws_s3_bucket_public_access_block" "tf_state" {
   restrict_public_buckets = true
 }
 
-# DynamoDB table for state locking
-resource "aws_dynamodb_table" "tf_locks" {
-  name         = "terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute { 
-    name = "LockID" 
-  type = "S" 
-  }
-}
-
 output "state_bucket"  { value = aws_s3_bucket.tf_state.bucket }
 output "lock_table"    { value = aws_dynamodb_table.tf_locks.name }

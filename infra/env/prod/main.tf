36c323ccc8d1050dd1d928e5f1b4ac6da90c3c39
1Ridwan <97636059+1Ridwan@users.noreply.github.com>
2026-01-17 15:37:04 +0000

    terraform fmt

--- infra/env/prod/main.tf
+++ infra/env/prod/main.tf
@@ -1,26 +1,26 @@
 module "vpc" {
-  source             = "../../modules/vpc"
-  vpc_cidr_block     = var.vpc_cidr_block
-  vpc_region         = var.vpc_region
-  ecs_service_sg_id  = module.sg.ecs_service_sg_id
-  endpoint_sg_id     = module.sg.endpoint_sg_id
-  env_name = var.env_name
-  number_of_azs = var.number_of_azs
+  source            = "../../modules/vpc"
+  vpc_cidr_block    = var.vpc_cidr_block
+  vpc_region        = var.vpc_region
+  ecs_service_sg_id = module.sg.ecs_service_sg_id
+  endpoint_sg_id    = module.sg.endpoint_sg_id
+  env_name          = var.env_name
+  number_of_azs     = var.number_of_azs
 }
 
 module "ecs" {
-  source             = "../../modules/ecs"
-  vpc_id             = module.vpc.vpc_id
-  private_subnet_ids = module.vpc.private_subnet_ids
-  ecs_service_sg_id  = module.sg.ecs_service_sg_id
-  container_port     = var.container_port
-  ecr_repo_name = var.ecr_repo_name
-  task_count = var.task_count
-  env_name = var.env_name
-  table_name = var.table_name
-  cpu_size = var.cpu_size
-  memory_size = var.memory_size
-  blue_target_group_arn  = module.alb.blue_target_group_arn
+  source                = "../../modules/ecs"
+  vpc_id                = module.vpc.vpc_id
+  private_subnet_ids    = module.vpc.private_subnet_ids
+  ecs_service_sg_id     = module.sg.ecs_service_sg_id
+  container_port        = var.container_port
+  ecr_repo_name         = var.ecr_repo_name
+  task_count            = var.task_count
+  env_name              = var.env_name
+  table_name            = var.table_name
+  cpu_size              = var.cpu_size
+  memory_size           = var.memory_size
+  blue_target_group_arn = module.alb.blue_target_group_arn
 }
 
 module "alb" {
@@ -32,7 +32,7 @@ module "alb" {
   container_port    = var.container_port
   test_traffic_port = var.test_traffic_port
   apex_domain       = var.apex_domain
-  env_name = var.env_name
+  env_name          = var.env_name
 }
 
 module "sg" {
@@ -46,21 +46,21 @@ module "acm" {
   apex_domain  = var.apex_domain
   alb_dns_name = module.alb.alb_dns_name
   alb_zone_id  = module.alb.alb_zone_id
-  env_name = var.env_name
+  env_name     = var.env_name
 }
 
 module "ddb" {
-  source       = "../../modules/ddb"
+  source   = "../../modules/ddb"
   env_name = var.env_name
 }
 
 module "codedeploy" {
-  source       = "../../modules/codedeploy"
-  env_name = var.env_name
-  ecs_cluster_name = module.ecs.ecs_cluster_name
-  ecs_service_name = module.ecs.ecs_service_name
-  prod_listener_arn  = module.alb.prod_listener_arn
-  test_listener_arn  = module.alb.test_listener_arn
+  source                  = "../../modules/codedeploy"
+  env_name                = var.env_name
+  ecs_cluster_name        = module.ecs.ecs_cluster_name
+  ecs_service_name        = module.ecs.ecs_service_name
+  prod_listener_arn       = module.alb.prod_listener_arn
+  test_listener_arn       = module.alb.test_listener_arn
   blue_target_group_name  = module.alb.blue_target_group_name
   green_target_group_name = module.alb.green_target_group_name
 }

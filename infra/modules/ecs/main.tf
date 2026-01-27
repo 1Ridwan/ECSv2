37919c14feda38751ae78952be38f7de85df2ac7
1Ridwan <97636059+1Ridwan@users.noreply.github.com>
2026-01-27 01:55:29 +0000

    add env specific name to task definition

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -61,7 +61,7 @@ resource "aws_ecs_service" "main" {
 
 # TODO: pass TABLE_NAME to task definition through vault?
 resource "aws_ecs_task_definition" "app_task" {
-  family                   = "url-service"
+  family                   = "${var.env_name}-url-service"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = var.cpu_size

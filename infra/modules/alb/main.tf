75917cc25442d76dce20f1d610434aac8d7af432
1Ridwan <97636059+1Ridwan@users.noreply.github.com>
2026-01-27 23:26:41 +0000

    Merge main with dev (#58)

--- infra/modules/alb/main.tf
+++ infra/modules/alb/main.tf
@@ -1,7 +1,7 @@
 # create public facing load balancer
 
 resource "aws_lb" "main" {
-  name               = "alb-test-v7-${var.env_name}"
+  name               = "alb-${var.env_name}"
   internal           = false
   load_balancer_type = "application"
   security_groups    = var.alb_sg
@@ -78,4 +78,50 @@ resource "aws_lb_listener" "front_end_http" {
       status_code = "HTTP_301"
     }
   }
+}
+
+# WAFv2 for the alb
+# AWS Managed Core Rule Set provides essential protections such as preventing:
+# SQL Injection, Cross-Site Scripting, HTTP flood attacks, scanning probes and more
+
+resource "aws_wafv2_web_acl" "main" {
+  name  = "${var.env_name}-waf-web-acl"
+  scope = "REGIONAL"
+
+  default_action {
+    allow {}
+  }
+
+  rule {
+    name     = "AWSManagedRulesCommonRuleSet"
+    priority = 0
+
+    override_action {
+      count {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesCommonRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "webACLVisibilityConfig"
+    sampled_requests_enabled   = true
+  }
+}
+
+resource "aws_wafv2_web_acl_association" "main" {
+  resource_arn = aws_lb.main.arn
+  web_acl_arn  = aws_wafv2_web_acl.main.arn
 }
